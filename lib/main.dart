68de2241c83ec6ca7056f43b46bc836a12d5bb83
KishorKumarK19 <kkishorkumar0007@gmail.com>
2023-07-10 22:48:16 +0530

    Refactored : Name changed back

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
+      title: 'Flutter Demo',
       routes: {
         //'/': (context) => Register(),
         '/': (context) => Register(),
